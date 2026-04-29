1ebe2b5352cbd8ae9eced1e1656ec332a339173f
luckysany42-netizen <luckysany42@gmail.com>
2026-04-29 11:37:34 +0700

    Add Vite directive to load Vue.js app and assets

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name', 'KyPay') }}</title>
+    @vite(['resources/css/app.css', 'resources/js/main.ts'])
 </head>
 <body>
     <div id="app"></div>
