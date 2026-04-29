8c042d485caf0189bdbf592d09e7c71f70fd9ab3
luckysany42-netizen <luckysany42@gmail.com>
2026-04-29 11:46:14 +0700

    Add TrustProxies middleware to detect HTTPS from Railway proxy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->use([
+            \App\Http\Middleware\TrustProxies::class,
+        ]);
+
         $middleware->alias([
             'auth.token' => \App\Http\Middleware\AuthTokenMiddleware::class,
             'role'       => \App\Http\Middleware\RoleMiddleware::class,
