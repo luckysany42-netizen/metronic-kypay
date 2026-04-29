07f32a708b250a9abaafb6bec938466c3e690c1c
luckysany42-netizen <luckysany42@gmail.com>
2026-04-29 11:13:27 +0700

    Fix: remove vite from blade

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,9 +4,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name', 'KyPay') }}</title>
-    @if(app()->environment('local') || file_exists(public_path('build/manifest.json')))
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    @endif
 </head>
 <body>
     <div id="app"></div>
